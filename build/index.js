51771df9f1ed99acd33d1042f2c5d99a426b3c6b
Maldercito <miky.rl@gmail.com>
2017-02-17 09:12:51 +0100

    build and fix

--- build/index.js
+++ build/index.js
@@ -1,17 +1,16 @@
 'use strict';
 
-const userKey = process.argv[2];
+var userKey = process.argv[2];
 
 if (userKey) {
     if (userKey.length >= 24) {
-        const fs = require('fs');
-        let config = require('./config');
+        var fs = require('fs');
+        var config = require('./config');
         config.liberiot.user_key = userKey;
         fs.writeFileSync("./config.json", JSON.stringify(config, null, 2));
         console.log('Using user key ' + userKey);
     }
 }
 
-
-const mqttBroker = require('./src/mqtt/moscaBroker');
-const app = require('./src/app');
+var mqttBroker = require('./src/mqtt/moscaBroker');
+var app = require('./src/app');
